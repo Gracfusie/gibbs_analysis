d2c02ace0de0afa0b49035d5f81ef5337d735651
Gracfusie <109655820+Gracfusie@users.noreply.github.com>
2025-03-20 20:59:40 +0800

    fix runbug

--- include/fixed_point.h
+++ include/fixed_point.h
@@ -50,7 +50,13 @@
 	#define _USE_MATH_DEFINES
 	#define __FPML_DEFINED_USE_MATH_DEFINES__
 #endif
-#include <math.h>
+// #include <math.h>
+#include <cmath>
+
+#ifndef M_PI
+    #define M_PI 3.14159265358979323846
+#endif
+
 #include <errno.h>
 
 namespace fpml {
